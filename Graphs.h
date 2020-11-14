fdab83bce88ebf3b4360332c4c6a52d3e1c08dbc
Marsh <pontiacamarshal@gmail.com>
2020-11-14 12:50:01 -0500

    finished

---
+++ Graphs.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <iostream>
+
+#define MATRIX_SIZE 100
+
+class Graph
+{
+public:
+    Graph(int num_v);
+    ~Graph() {};
+
+    bool addEdge(int n_1, int n_2); // Add edge (i,j) to E
+
+    bool removeEdge(int n_1, int n_2); // Remove the edge (i,j) from E
+
+    bool hasEdge(int n_1, int n_2); // check if edge (i,j) is in E
+
+    std::vector<int> outEdges(int n); // returns list of ints such that (i,j) is in E
+
+    std::vector<int> inEdges(int n); // returns list of ints such that (j,i) is in E
+
+    std::string printGraph(); // Prints adjacency matrix
+    
+    bool isEmpty();
+
+private:
+    Graph(); // Unused
+    int m_matrix_size;
+    bool m_adj_matrix[MATRIX_SIZE][MATRIX_SIZE];
+
+};
--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,166 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
 #include "Graphs.cpp"
 
 using namespace std;
 
 int main()
 {
+    bool run = true;
+    int input = 0;
+    int n_1, n_2, n;
+    std::vector<int> edges; 
+
+    Graph graph(10);
+    
+    while(run)
+    {
+        std::cout <<
+        "Press 1 to add an edge to graph" << std::endl << 
+        "Press 2 remove an edge from graph" << std::endl << 
+        "Press 3 Find an edge in the graph" << std::endl << 
+        "Press 4 Find the out edges of a vertices" << std::endl << 
+        "Press 5 Find the in edges of a vertices" << std::endl << 
+        "Press 6 To Print out Adjacency Matrix" << std::endl << 
+        "Press 7 to quit" << std::endl; 
+
+        std::cin >> input;
+        switch(input)
+        {
+            case 1:
+                // Get input
+                std::cout << "Enter the first node:" << std::endl;
+                std::cin >> n_1;
+                std::cout << "Enter the second node:" << std::endl;
+                std::cin >> n_2;
+
+                // Check if valid input
+                if ((n_1 > 100 || n_1 < 0 ) || (n_2 > 100 || n_2 < 0))
+                {
+                    std::cout << "Invalid nodes\n";
+                    break;
+                }
+
+                // add the edge
+                if (graph.addEdge(n_1, n_2))
+                    printf("Added edge from %d to %d\n", n_1, n_2);
+                else
+                    printf("Failed to add edge\n");
+                
+                break;
+                
+            case 2:
+                // Get input
+                std::cout << "Enter the first node:" << std::endl;
+                std::cin >> n_1;
+                std::cout << "Enter the second node:" << std::endl;
+                std::cin >> n_2;
+
+                // Check if valid input
+                if ((n_1 > 100 || n_1 < 0 ) || (n_2 > 100 || n_2 < 0))
+                {
+                    std::cout << "Invalid nodes\n";
+                    break;
+                }
+
+                // remove the edge
+                if (graph.removeEdge(n_1, n_2))
+                    printf("Removed edge from %d to %d\n", n_1, n_2);
+                else
+                    printf("Failed to remove edge\n");
+                
+                break;
+
+            case 3:
+            
+                // Get input
+                std::cout << "Enter the first node:" << std::endl;
+                std::cin >> n_1;
+                std::cout << "Enter the second node:" << std::endl;
+                std::cin >> n_2;
+
+                // Check if valid input
+                if ((n_1 > 100 || n_1 < 0 ) || (n_2 > 100 || n_2 < 0))
+                {
+                    std::cout << "Invalid nodes\n";
+                    break;
+                }
+
+                // Find the edge
+                if (graph.hasEdge(n_1, n_2))
+                    printf("Edge FOUND from %d to %d\n", n_1, n_2);
+                else
+                    printf("Edge NOT FOUND from %d to %d\n", n_1, n_2);
+                
+                break;
+
+            case 4:
+            
+                // Get input
+                std::cout << "Enter the node of interest:" << std::endl;
+                std::cin >> n;
+
+                // Check if valid input
+                if (n > 100 || n < 0 )
+                {
+                    std::cout << "Invalid node\n";
+                    break;
+                }
+
+                // Find out-edges
+                edges = graph.outEdges(n);
+                printf("Found nodes pointing outward towards %d\n", n);
+                for (auto i : edges)
+                {
+                    std::cout << i << std::endl;
+                }
+                
+                break;
+
+            case 5:
+                // Get input
+                std::cout << "Enter the node of interest:" << std::endl;
+                std::cin >> n;
+
+                // Check if valid input
+                if (n > 100 || n < 0 )
+                {
+                    std::cout << "Invalid node\n";
+                    break;
+                }
+
+                // Find in-edges
+                edges = graph.inEdges(n);
+                printf("Found nodes pointing inward towards %d\n", n);
+                for (auto i : edges)
+                {
+                    std::cout << i << std::endl;
+                }
+                
+                break;
+                
+            case 6:
+                std::cout << "Printing Matrix\n";
+                if (graph.isEmpty())
+                {
+                    std::cout << "Graph is empty" << endl;
+                    break;
+                }
+                graph.printGraph(); 
+                break;
+            
+            case 7:
+                run = false;
+                break;
+            
+            default:
+                std::cout << "Invalid input\n";
+                break;
+        }
+    }
+
+    std::cout << "Thanks for playing\n";
+
     return EXIT_SUCCESS;
 }
